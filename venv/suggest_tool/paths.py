ac9d20f826881b22a3a825708f63a3d697180c41
kot <rogachev.ga99@gmail.com>
2020-04-21 08:34:16 +0300

    s

--- venv/suggest_tool/paths.py
+++ venv/suggest_tool/paths.py
@@ -1,17 +1,17 @@
 import pickle
 
-USERS_PATH = 'venv/suggest_tool/tables/users.pck'
+USERS_PATH = '/home/kotsmile/mysite/venv/suggest_tool/tables/users.pck'
 
-DAY_MENU_PATH = 'venv/suggest_tool/tables/day_menu.pck'
+DAY_MENU_PATH = '/home/kotsmile/mysite/venv/suggest_tool/tables/day_menu.pck'
 
-ACTIVITY_LEVELS_PATH =  'venv/suggest_tool/tables/activity_level.pck'
-GOALS_PATH =  'venv/suggest_tool/tables/goals.pck'
-PERIODS_PATH =  'venv/suggest_tool/tables/periods.pck'
-EQ_CONF_PATH = 'venv/suggest_tool/tables/eq_conf.pck'
-RECIPES_PATH = 'venv/suggest_tool/tables/recipes.pck'
-CODE_RECIPES_PATH =  'venv/suggest_tool/tables/code_recipes.pck'
+ACTIVITY_LEVELS_PATH =  '/home/kotsmile/mysite/venv/suggest_tool/tables/activity_level.pck'
+GOALS_PATH =  '/home/kotsmile/mysite/venv/suggest_tool/tables/goals.pck'
+PERIODS_PATH =  '/home/kotsmile/mysite/venv/suggest_tool/tables/periods.pck'
+EQ_CONF_PATH = '/home/kotsmile/mysite/venv/suggest_tool/tables/eq_conf.pck'
+RECIPES_PATH = '/home/kotsmile/mysite/venv/suggest_tool/tables/recipes.pck'
+CODE_RECIPES_PATH =  '/home/kotsmile/mysite/venv/suggest_tool/tables/code_recipes.pck'
 
-UPDATE_PATH = 'venv/suggest_tool/tables/update.pck'
+UPDATE_PATH = '/home/kotsmile/mysite/venv/suggest_tool/tables/update.pck'
 
 
 
